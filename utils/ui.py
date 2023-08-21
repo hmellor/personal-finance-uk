ce3b520b77c1539ec684cde3c1f1dcfce32544f2
HMellor <hej.mellor@gmail.com>
2023-08-21 19:13:53 +0100

    Make `ui.py` functional

--- utils/ui.py
+++ utils/ui.py
@@ -3,131 +3,106 @@ from datetime import date
 
 import ipywidgets as widgets
 import pandas as pd
+import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 from utils.tax import net_present_value
 
-pd.options.plotting.backend = "plotly"
 
-style = {"description_width": "22%"}
-layout = widgets.Layout(width="auto")
+def create_inputs():
+    inputs = {}
+    style = {"description_width": "22%"}
+    layout = widgets.Layout(width="auto")
+    slider_kwargs = dict(style=style, layout=layout, continuous_update=False)
+
+    inputs["plan"] = widgets.ToggleButtons(
+        value=os.getenv("PLAN", "Plan 2"),
+        options=["Plan 1", "Plan 2", "Plan 4", "Plan 5", "Postgraduate"],
+        tooltips=[
+            "If you started your course before 1 September 2012",
+            "If you started your course between 1 September 2012 and 31 July 2023",
+            "If you applied to Student Awards Agency Scotland",
+            "If you started your course on or after 1 August 2023",
+            "If you studdied a postgraduate master's or doctoral course",
+        ],
+        layout=layout,
+    )
+
+    inputs["graduation_year"] = widgets.IntSlider(
+        description="Graduation year",
+        value=int(os.getenv("GRADUATION_YEAR", date.today().year)),
+        min=2000,
+        max=2050,
+        **slider_kwargs,
+    )
+
+    inputs["loan"] = widgets.FloatSlider(
+        description="Loan (£)",
+        value=float(os.getenv("LOAN", 45000)),
+        max=150000,
+        step=100,
+        **slider_kwargs,
+    )
+
+    inputs["interest_rate"] = widgets.FloatSlider(
+        description="Interest rate (%/year)",
+        value=float(os.getenv("INTEREST_RATE", 0.071)),
+        max=0.2,
+        step=0.001,
+        readout_format=".1%",
+        **slider_kwargs,
+    )
+
+    inputs["initial_salary"] = widgets.FloatSlider(
+        description="Initial salary (£)",
+        value=float(os.getenv("INITIAL_SALARY", 30000)),
+        max=150000,
+        step=100,
+        **slider_kwargs,
+    )
+
+    inputs["salary_sacrifice"] = widgets.FloatSlider(
+        description="Salary sacrifice (%/month)",
+        value=float(os.getenv("SALARY_SACRIFICE", 0.0)),
+        max=0.5,
+        step=0.01,
+        readout_format=".0%",
+        **slider_kwargs,
+    )
+
+    inputs["salary_growth"] = widgets.FloatSlider(
+        description="Salary growth (%/year)",
+        value=float(os.getenv("SALARY_GROWTH", 0.08)),
+        max=0.5,
+        step=0.01,
+        readout_format=".0%",
+        **slider_kwargs,
+    )
+
+    inputs["inflation_rate"] = widgets.FloatSlider(
+        description="Inflation rate (%/year)",
+        value=float(os.getenv("INFLATION_RATE", 0.04)),
+        max=0.2,
+        step=0.001,
+        readout_format=".1%",
+        **slider_kwargs,
+    )
+
+    inputs["extra_repayments"] = widgets.FloatSlider(
+        description="Extra repayments (£/month)",
+        value=0,
+        max=2000,
+        step=10,
+        readout_format=".2f",
+        **slider_kwargs,
+    )
+
+    return inputs
 
 
-plan = widgets.ToggleButtons(
-    value=os.getenv("PLAN", "Plan 2"),
-    options=["Plan 1", "Plan 2", "Plan 4", "Plan 5", "Postgraduate"],
-    tooltips=[
-        "If you started your course before 1 September 2012",
-        "If you started your course between 1 September 2012 and 31 July 2023",
-        "If you applied to Student Awards Agency Scotland",
-        "If you started your course on or after 1 August 2023",
-        "If you studdied a postgraduate master's or doctoral course",
-    ],
-    layout=layout,
-)
-
-graduation_year = widgets.IntSlider(
-    description="Graduation year",
-    style=style,
-    value=int(os.getenv("GRADUATION_YEAR", date.today().year)),
-    min=2000,
-    max=2050,
-    continuous_update=False,
-    layout=layout,
-)
-
-loan = widgets.FloatSlider(
-    description="Loan (£)",
-    style=style,
-    value=float(os.getenv("LOAN", 45000)),
-    max=150000,
-    step=100,
-    continuous_update=False,
-    layout=layout,
-)
-
-interest_rate = widgets.FloatSlider(
-    description="Interest rate (%/year)",
-    style=style,
-    value=float(os.getenv("INTEREST_RATE", 0.071)),
-    max=0.2,
-    step=0.001,
-    continuous_update=False,
-    readout_format=".1%",
-    layout=layout,
-)
-
-initial_salary = widgets.FloatSlider(
-    description="Initial salary (£)",
-    style=style,
-    value=float(os.getenv("INITIAL_SALARY", 30000)),
-    max=150000,
-    step=100,
-    continuous_update=False,
-    layout=layout,
-)
-
-salary_sacrifice = widgets.FloatSlider(
-    description="Salary sacrifice (%/month)",
-    style=style,
-    value=float(os.getenv("SALARY_SACRIFICE", 0.0)),
-    max=0.5,
-    step=0.01,
-    continuous_update=False,
-    readout_format=".0%",
-    layout=layout,
-)
-
-salary_growth = widgets.FloatSlider(
-    description="Salary growth (%/year)",
-    style=style,
-    value=float(os.getenv("SALARY_GROWTH", 0.08)),
-    max=0.5,
-    step=0.01,
-    continuous_update=False,
-    readout_format=".0%",
-    layout=layout,
-)
-
-inflation_rate = widgets.FloatSlider(
-    description="Inflation rate (%/year)",
-    style=style,
-    value=float(os.getenv("INFLATION_RATE", 0.04)),
-    max=0.2,
-    step=0.001,
-    continuous_update=False,
-    readout_format=".1%",
-    layout=layout,
-)
-
-extra_repayments = widgets.FloatSlider(
-    description="Extra repayments (£/month)",
-    style=style,
-    value=0,
-    max=2000,
-    step=10,
-    continuous_update=False,
-    readout_format=".2f",
-    layout=layout,
-)
-
-inputs = {
-    "plan": plan,
-    "graduation_year": graduation_year,
-    "loan": loan,
-    "interest_rate": interest_rate,
-    "inflation_rate": inflation_rate,
-    "initial_salary": initial_salary,
-    "salary_sacrifice": salary_sacrifice,
-    "salary_growth": salary_growth,
-    "extra_repayments": extra_repayments,
-}
-
-controls = widgets.VBox(children=tuple(inputs.values()))
-
-fig = go.FigureWidget(
-    make_subplots(
+def create_figure():
+    fig = make_subplots(
         rows=1,
         cols=2,
         shared_xaxes="all",
@@ -136,28 +111,30 @@ fig = go.FigureWidget(
         x_title="Date",
         y_title="Amount (£)",
     )
-)
-fig.update_layout(
-    margin=dict(l=60, r=20, t=80, b=60),
-    legend=dict(
-        orientation="h",
-        yanchor="bottom",
-        y=1.02,
-        xanchor="center",
-        x=0.5,
-    ),
-)
-fig.update_yaxes(tickprefix="£", nticks=10, rangemode="nonnegative")
+    fig.update_layout(
+        margin=dict(l=60, r=20, t=80, b=60),
+        legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="center",
+            x=0.5,
+        ),
+    )
+    fig.update_yaxes(tickprefix="£", nticks=10, rangemode="nonnegative")
+    return fig
 
-app = widgets.VBox(
-    children=[
-        controls,
-        fig,
-    ],
-)
+
+def observe_children(widget, callback):
+    for child in widget.children:
+        if hasattr(child, "children"):
+            observe_children(child, callback)
+        else:
+            child.observe(callback, names="value")
 
 
 def plot(
+    fig: go.FigureWidget,
     data: pd.DataFrame,
     inflation_rate: float = 0.05,
 ):
@@ -173,7 +150,7 @@ def plot(
     title_text = f"Outstanding balance NPV: £{final_loan_npv:,.2f}, Repayment months: {len(data)}, Repayment NPV: £{total_repayment_npv:,.2f}"
 
     # Plot lines
-    lines = data.plot()
+    lines = px.line(data, x=data.index, y=data.columns)
     with fig.batch_update():
         # Add/update data
         if not fig.data:
