5ef47144c980a383be5c69677b9e1857b89e3692
HMellor <hej.mellor@gmail.com>
2024-01-15 14:54:32 +0000

    Add docstrings to `ui.py`

--- utils/ui.py
+++ utils/ui.py
@@ -5,12 +5,20 @@ import ipywidgets as widgets
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.graph_objs._figure import Figure
 from plotly.subplots import make_subplots
 
 from utils.tax import net_present_value
 
 
-def create_inputs():
+def create_inputs() -> dict[str, widgets.Widget]:
+    """
+    This function creates and returns a dictionary of widgets that will be used to get user inputs for the calculation.
+    The widgets created include ToggleButtons, IntSlider, and FloatSlider, each representing different parameters needed for the calculation.
+
+    Returns:
+        dict: A dictionary of widgets for user inputs.
+    """
     inputs = {}
     style = {"description_width": "22%"}
     layout = widgets.Layout(width="auto")
@@ -101,13 +109,20 @@ def create_inputs():
     return inputs
 
 
-def create_figure():
+def create_figure() -> Figure:
+    """
+    This function creates a subplot figure with two columns using the Plotly library. Both plots share the same x-axis.
+    The x-axis represents the date and the y-axis represents the amount in pounds. The function also sets the layout
+    and updates the y-axis properties.
+
+    Returns:
+        fig: A Plotly figure with two subplots.
+    """
     fig = make_subplots(
         rows=1,
         cols=2,
         shared_xaxes="all",
         horizontal_spacing=0.1,
-        # vertical_spacing=0,
         x_title="Date",
         y_title="Amount (£)",
     )
@@ -125,7 +140,15 @@ def create_figure():
     return fig
 
 
-def observe_children(widget, callback):
+def observe_children(widget: widgets.Widget, callback: callable) -> None:
+    """
+    This function recursively observes changes in the 'value' attribute of the given widget and its children.
+    When a change is observed, the provided callback function is triggered.
+
+    Args:
+        widget (widgets.Widget): The widget to observe.
+        callback (callable): The function to be called when the 'value' attribute of the widget or its children changes.
+    """
     for child in widget.children:
         if hasattr(child, "children"):
             observe_children(child, callback)
@@ -138,6 +161,18 @@ def plot(
     data: pd.DataFrame,
     inflation_rate: float = 0.05,
 ):
+    """
+    This function plots the loan repayment data on a Plotly figure widget.
+
+    Args:
+        fig (go.FigureWidget): The Plotly figure widget to plot the data on.
+        data (pd.DataFrame): The data to plot. The DataFrame should have columns for the loan, salary repayment, and extra repayment.
+        inflation_rate (float, optional): The inflation rate to use for calculating the net present value. Defaults to 0.05.
+
+    The function first calculates the net present value of the final loan and the total repayment. It then creates a title text
+    for the plot with these values. Afterward, it plots the data on the figure widget. If the figure already has data, the function
+    updates the existing data with the new data. Finally, it updates the title of the figure.
+    """
     # Create title
     final_loan_npv = net_present_value(data["loan"], discount_rate=inflation_rate / 12)[
         -1
